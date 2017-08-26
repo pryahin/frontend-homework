c8b460deb3c3a3e64d3939c5ce99c724cfb4a5b1
pryahin <pryahin.v@gmail.com>
2017-08-26 23:20:04 +0300

    Update

--- source/roman.js
+++ source/roman.js
@@ -1,35 +1,16 @@
 'use strict';
 
 function roman(val) {
-    if (/^\d+$/.test(val)) {
-        return toRoman(Number(val));
-    } else {
-        return fromRoman(val);
-    }
+    return (/^\d+$/.test(val)) ? toRoman(+val) : fromRoman(val);
 }
 
 function toRoman(val) {
-    let map = {
-        '+1000': 'M', //+ чтобы в for..in свойства перечислялись в заданном мной порядке
-        '+900': 'CM',
-        '+500': 'D',
-        '+400': 'CD',
-        '+100': 'C',
-        '+90': 'XC',
-        '+50': 'L',
-        '+40': 'XL',
-        '+10': 'X',
-        '+9': 'IX',
-        '+5': 'V',
-        '+4': 'IV',
-        '+1': 'I'
-    }
     let result = '';
     while (val > 0) {
-        for (let key in map) {
-            if (val >= Number(key)) {
-                result += map[key];
-                val -= Number(key);
+        for (let key in mapToRoman) {
+            if (val >= +key) {
+                result += mapToRoman[key];
+                val -= +key;
                 break;
             }
         }
@@ -38,20 +19,37 @@ function toRoman(val) {
 }
 
 function fromRoman(val) {
-    let map = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
     let result = 0;
     val.toUpperCase().split('').reduce((prev, current) => {
-        let curr = map[current];
+        let curr = mapFromRoman[current];
         result += prev < curr ? curr - 2 * prev : curr;
         return curr;
     }, 0);
     return result;
+}
+
+const mapToRoman = {
+    '+1000': 'M', //+ для того, чтобы в for..in свойства перечислялись в заданном мной порядке
+    '+900': 'CM',
+    '+500': 'D',
+    '+400': 'CD',
+    '+100': 'C',
+    '+90': 'XC',
+    '+50': 'L',
+    '+40': 'XL',
+    '+10': 'X',
+    '+9': 'IX',
+    '+5': 'V',
+    '+4': 'IV',
+    '+1': 'I'
+}
+
+const mapFromRoman = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
 }
