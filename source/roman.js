1b199cf58aa838d9ab74e9254352911d967774b1
pryahin <pryahin.v@gmail.com>
2017-08-27 00:08:16 +0300

    Update2

--- source/roman.js
+++ source/roman.js
@@ -6,50 +6,45 @@ function roman(val) {
 
 function toRoman(val) {
     let result = '';
+
     while (val > 0) {
-        for (let key in mapToRoman) {
-            if (val >= +key) {
-                result += mapToRoman[key];
-                val -= +key;
+        for (let key in mapRoman) {
+            if (val >= mapRoman[key]) {
+                result += key;
+                val -= mapRoman[key];
                 break;
             }
         }
     }
+    
     return result;
 }
 
 function fromRoman(val) {
     let result = 0;
+
     val.toUpperCase().split('').reduce((prev, current) => {
-        let curr = mapFromRoman[current];
+        let curr = mapRoman[current];
         result += prev < curr ? curr - 2 * prev : curr;
+
         return curr;
     }, 0);
+
     return result;
 }
 
-const mapToRoman = {
-    '+1000': 'M', //+ для того, чтобы в for..in свойства перечислялись в заданном мной порядке
-    '+900': 'CM',
-    '+500': 'D',
-    '+400': 'CD',
-    '+100': 'C',
-    '+90': 'XC',
-    '+50': 'L',
-    '+40': 'XL',
-    '+10': 'X',
-    '+9': 'IX',
-    '+5': 'V',
-    '+4': 'IV',
-    '+1': 'I'
-}
-
-const mapFromRoman = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
+const mapRoman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+};
